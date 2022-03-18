805435fe74550073039d70e62ae0168efbded538
wutongliuxue <wutongliuxue@163.com>
2022-03-18 12:38:38 -0400

    Add phase 2 project

---
+++ Project Version 4.0.2/Assets/OutsideMap/C# Folder/CameraController.cs
@@ -0,0 +1,26 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Camera : MonoBehaviour
+// {
+
+//     private Vector3 offset;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         offset = transform.position;
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+
+//     void LateUpdate(){
+//         transform.position = Camera.transorm.position + offset;
+//     }
+// }
+
